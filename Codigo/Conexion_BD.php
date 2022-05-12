e4999bd411f10cb951a85b6ee6e05ade0f3053e0
abrahamelfahsi <abrahamelfahsi@gmail.com>
2022-05-12 21:01:43 +0200

    modificando despliegue

--- Codigo/Conexion_BD.php
+++
@@ -1,9 +0,0 @@
-<?php
-        //Lo primero debemos incluir el fichero donde esta el conector
-        require 'BD/ConectorBD.php';
-        echo "Se ha cargado correctamente el archivo <br>";
-
-        //Queremos conectarnos con la BD. 
-        $conexion = conectar(false);
-
-?>
--- Codigo/prueba.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'BD/ConectorBD.php';
-$conexion=conectar(false);
-$consulta="select * from usuario";
-$con=mysqli_query($conexion,$consulta);
-while($usu=mysqli_fetch_assoc($con)){
-    echo $usu['nombre'];
-}
-?>
